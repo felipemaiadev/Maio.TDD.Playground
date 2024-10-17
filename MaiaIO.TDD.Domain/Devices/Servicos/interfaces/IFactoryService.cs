aaf4c39b90b140ea0589649a80b29e39a4326a56
felipemaia <felipe.maia.rj@gmai.com>
2024-10-17 05:04:48 -0300

    Ajustes - Solucao

---
+++ MaiaIO.TDD.Domain/Devices/Servicos/interfaces/IFactoryService.cs
@@ -0,0 +1,18 @@
+﻿using MaiaIO.TDD.Domain.Factories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiaIO.TDD.Domain.Devices.Servicos.interfaces
+{
+    public interface IFactoryService
+    {
+        Factory InsertAsync(Factory factory);
+        Factory UpdateAsync(Factory factory);
+        Factory DeleteAsync(Factory factory);
+        IEnumerable<Factory> GetListAsync(); 
+        Factory GetByIdAsync(long id);
+    }
+}
